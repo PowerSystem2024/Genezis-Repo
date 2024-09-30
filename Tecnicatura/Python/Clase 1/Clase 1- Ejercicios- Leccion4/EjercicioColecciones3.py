75b82ac20e7744efa0328a716613ab11d35b6d75
Gabriel Garino <gabrielgarino10@gmail.com>
2024-09-30 19:18:39 -0300

    Agregando los ejercicios de la clase 1 de Python
    Agregando los ejercicios de la clase 1 de Python al repositorio

---
+++ Tecnicatura/Python/Clase 1/Clase 1- Ejercicios- Leccion4/EjercicioColecciones3.py
@@ -0,0 +1,35 @@
+# Ejercicio 3: Agregar personajes a una lista
+# Escriba un programa donde cree una lista con los siguientes personajes del señor de los anillos
+# Nombre: Aragorn
+# Clase: Guerrero
+# Raza: Dúnadan del Norte
+
+# Nombre: Gandalf 
+# Clase: Mago
+# Raza: Istar
+
+# Nombre: Legolas
+# Clase: Arquero
+# Raza: Elfo Sindar
+
+personajes = [] # Creamos una lista vacia
+# Creamos diccionario
+P = {'Nombre': 'Aragorn', 'Clase': 'Guerrero', 'Raza': 'Dunadan del Norte'}
+personajes.append(P) # Agregamos a la lista un personaje
+P = {'Nombre': 'Gandalf', 'Clase': 'Mago', 'Raza': 'Istari'}
+personajes.append(P)
+P = {'Nombre': 'Legolas', 'Clase': 'Arquero', 'Raza': 'Elfo Sindar'}
+personajes.append(P)
+P = {'Nombre': 'Frodo', 'Clase': 'Aventurero', 'Raza': 'Hobbit'}
+personajes.append(P)
+P = {'Nombre': 'Gimli', 'Clase': 'Guerrero', 'Raza': 'Enano'}
+personajes.append(P)
+P = {'Nombre': 'Saruman', 'Clase': 'Mago', 'Raza': 'Istari'}
+personajes.append(P)
+print(personajes) # Tarea: Agregar por lo menos otros tres personajes, que sean a tu elección
+
+
+
+
+
+
