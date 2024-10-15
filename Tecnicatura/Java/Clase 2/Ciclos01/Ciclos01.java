09269aa567658d072fe438851e1e94a93301cfc7
Lucianodavidcor <lucianocortez2003@gmail.com>
2024-10-15 15:06:58 -0300

    solucion codigo de clase 2 java

--- Tecnicatura/Java/Clase 2/Ciclos01/Ciclos01.java
+++ Tecnicatura/Java/Clase 2/Ciclos01/Ciclos01.java
@@ -8,17 +8,21 @@ import java.util.Scanner;
 
 public class Ciclos01 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        
-        int numero, cuadrado;
-        System.out.println("Digite un numero");
-        numero = Integer.parseInt(entrada.nextLine());
-        while(numero >= 0){
-            cuadrado = (int)Math.pow(numero, 2);
-            System.out.println("El numero "+numero+" -1elevado al cuadrado es: "+cuadrado);
-            System.out.println("Digite otro numero: ");
+        try (Scanner entrada = new Scanner(System.in)) {
+            int numero, cuadrado;
+            System.out.println("Digite un numero");
             numero = Integer.parseInt(entrada.nextLine());
+            while(numero >= 0){
+                cuadrado = (int)Math.pow(numero, 2);
+                System.out.println("El numero "+numero+" elevado al cuadrado es: "+cuadrado);
+                System.out.println("Digite otro numero: ");
+                numero = Integer.parseInt(entrada.nextLine());
+            }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        
         System.out.println("El progama a finalizado por numero negativo");
     }
 
